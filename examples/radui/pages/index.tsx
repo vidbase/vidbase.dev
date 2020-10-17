be8c060f6afe7f08d01fd36ba0b272ee7d78e4d5
Van Nguyen <thegoleffect@gmail.com>
2020-10-17 16:32:32 -0700

    update example/radui to have 100% lighthouse score, set up as typescript next.js project with typings

--- examples/radui/pages/index.tsx
+++ examples/radui/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
 import { ButtonProps } from '@radui/core'
@@ -11,11 +12,32 @@ const Button = dynamic<ButtonProps>(
 
 const IndexPage = () => {
 	return (
-		<div>
-			Hello <Button>Primary</Button>
-			<Button secondary>Secondary</Button>
-			<Button tertiary>Tertiary</Button>
-			<Button xl>Extra Large</Button>
+		<div style={{ display: 'grid', placeItems: 'center' }}>
+			<Head>
+				<title>@radui/core example</title>
+			</Head>
+			<div
+				style={{
+					display: 'flex',
+					width: '50%',
+					justifyContent: 'space-between',
+					margin: '150px auto'
+				}}>
+				<Button>Primary</Button>
+				<Button secondary>Secondary</Button>
+				<Button tertiary>Tertiary</Button>
+			</div>
+			<div
+				style={{
+					display: 'flex',
+					width: '50%',
+					justifyContent: 'space-between',
+					margin: '150px auto'
+				}}>
+				<Button sm>Small</Button>
+				<Button lg>Large</Button>
+				<Button xl>Extra Large</Button>
+			</div>
 		</div>
 	)
 }
