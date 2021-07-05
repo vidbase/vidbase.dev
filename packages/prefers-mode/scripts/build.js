f4861d0d66d5b717019fc57707f73ca2955c6291
Van Nguyen <thegoleffect@gmail.com>
2021-07-04 17:49:21 -0700

    move build script

--- packages/prefers-mode/scripts/build.js
+++ packages/prefers-mode/scripts/build.js
@@ -1,12 +1,10 @@
 const esbuild = require('esbuild')
 
 const options = {
-	entryPoints: ['src/index.tsx'],
+	entryPoints: ['./src/index.tsx'],
 	bundle: true,
-	minify: true,
 	external: ['react', 'react-dom'],
-	platform: 'node',
-	sourcemap: true
+	platform: 'node'
 }
 
 esbuild
@@ -22,6 +20,8 @@ esbuild
 esbuild
 	.build({
 		...options,
+		minify: true,
+		sourcemap: true,
 		outfile: './dist/prefers-mode.min.js'
 	})
 	.catch((err) => {
