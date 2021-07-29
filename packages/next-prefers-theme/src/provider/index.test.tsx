e32f067e1e07ef2d5c58f765473eda9470b30172
Tram Nguyen <thn099@ucsd.edu>
2021-07-28 20:47:58 -0700

    add tests for setPrefs

--- packages/next-prefers-theme/src/provider/index.test.tsx
+++ packages/next-prefers-theme/src/provider/index.test.tsx
@@ -2,10 +2,40 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import { PrefersThemeProvider, usePrefersTheme } from './index'
+import { useEffect } from 'react'
 
 function App() {
 	const { prefs, userPrefs } = usePrefersTheme()
 	return <div>{`Hello theme=${prefs.theme} (system: ${userPrefs.theme})`}</div>
+}
+
+function TestSetPrefs(props:any) {
+	const { prefs, userPrefs, setPrefs } = usePrefersTheme()
+	useEffect(() =>
+		setPrefs(props),
+		[]
+	)
+	if (props.theme) {
+		return <div>
+			{`Hello theme=${prefs.theme} (system: ${userPrefs.theme})`}
+		</div>
+	} else if (props.contrast) {
+		return <div>
+			{`Hello contrast=${prefs.contrast} (system: ${userPrefs.contrast})`}
+		</div>
+	} else if (props.data) {
+		return <div>
+			{`Hello data=${prefs.data} (system: ${userPrefs.data})`}
+	</div>
+	} else if (props.motion) {
+		return <div>
+			{`Hello motion=${prefs.motion} (system: ${userPrefs.motion})`}
+		</div>
+	} else {
+		return <div>
+			{`Default value=${prefs}`}
+		</div>
+	}
 }
 
 describe('PrefersThemeProvider', () => {
@@ -53,6 +83,78 @@ describe('PrefersThemeProvider', () => {
 			</PrefersThemeProvider>
 		)
 		expect(getByText('Hello theme=dark (system: null)')).toBeInTheDocument()
+		done()
+	})
+
+	it('use setPrefs to set theme=dark', (done) => {
+		const { getByText } = render(
+			<PrefersThemeProvider>
+				<TestSetPrefs theme='dark'/>
+			</PrefersThemeProvider>
+		)
+		expect(getByText('Hello theme=dark (system: dark)')).toBeInTheDocument()
+		done()
+	})
+
+	it('use setPrefs to set theme=light', (done) => {
+		const { getByText } = render(
+			<PrefersThemeProvider>
+				<TestSetPrefs theme='light'/>
+			</PrefersThemeProvider>
+		)
+		expect(getByText('Hello theme=light (system: light)')).toBeInTheDocument()
+		done()
+	})
+
+	it('use setPrefs to set contrast=more', (done) => {
+		const { getByText } = render(
+			<PrefersThemeProvider>
+				<TestSetPrefs contrast='more'/>
+			</PrefersThemeProvider>
+		)
+		expect(getByText('Hello contrast=more (system: more)')).toBeInTheDocument()
+		done()
+	})
+
+	it('use setPrefs to set contrast=less', (done) => {
+		const { getByText } = render(
+			<PrefersThemeProvider>
+				<TestSetPrefs contrast='less'/>
+			</PrefersThemeProvider>
+		)
+		expect(getByText('Hello contrast=less (system: less)')).toBeInTheDocument()
+		done()
+	})
+
+	it('use setPrefs to set motion=reduce', (done) => {
+		const { getByText } = render(
+			<PrefersThemeProvider>
+				<TestSetPrefs motion='reduce'/>
+			</PrefersThemeProvider>
+		)
+		expect(getByText('Hello motion=reduce (system: reduce)')).toBeInTheDocument()
+		done()
+	})
+
+	it('use setPrefs to set data=reduce', (done) => {
+		const { getByText } = render(
+			<PrefersThemeProvider>
+				<TestSetPrefs data='reduce'/>
+			</PrefersThemeProvider>
+		)
+		expect(getByText('Hello data=reduce (system: reduce)')).toBeInTheDocument()
+		done()
+	})
+
+	it('should throw an error', (done) => {
+		const spy = jest.spyOn(console, 'error'); // to prevent logging errors
+		spy.mockImplementation(() => {});
+		expect(() => render(
+			<PrefersThemeProvider>
+				<TestSetPrefs theme='invalid-value'/>
+			</PrefersThemeProvider>
+		)).toThrow()
+		spy.mockRestore();
 		done()
 	})
 })
